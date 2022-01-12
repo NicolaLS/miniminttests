c7040bbeabb384db89eaee416117f6235d4a7f65
elsirion <elsirion@protonmail.com>
2022-01-12 12:56:35 +0100

    mint-client: impl outgoing contract refund/claim input creation

--- mint-client/src/ln/outgoing.rs
+++ mint-client/src/ln/outgoing.rs
@@ -1,4 +1,6 @@
-use minimint::modules::ln::contracts::outgoing::OutgoingContract;
+use minimint::modules::ln::contracts::outgoing::{OutgoingContract, Preimage};
+use minimint::modules::ln::contracts::IdentifyableContract;
+use minimint::modules::ln::ContractInput;
 use minimint_api::encoding::{Decodable, Encodable};
 use minimint_api::Amount;
 
@@ -11,4 +13,23 @@ pub struct OutgoingContractData {
 pub struct OutgoingContractAccount {
     pub amount: Amount,
     pub contract: OutgoingContract,
+}
+
+#[allow(dead_code)]
+impl OutgoingContractAccount {
+    pub fn claim(&self, preimage: [u8; 32]) -> ContractInput {
+        ContractInput {
+            crontract_id: self.contract.contract_id(),
+            amount: self.amount,
+            witness: Some(Preimage(preimage)),
+        }
+    }
+
+    pub fn refund(&self) -> ContractInput {
+        ContractInput {
+            crontract_id: self.contract.contract_id(),
+            amount: self.amount,
+            witness: None,
+        }
+    }
 }
